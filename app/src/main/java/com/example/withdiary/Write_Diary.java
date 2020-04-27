68ad61d5d39aaf7075706e35048cea463d92299f
asudf <asudf@naver.com>
2020-04-27 10:54:15 +0900

    이미지 크롭
    오늘 저녁은 롤할꺼임

--- app/src/main/java/com/example/withdiary/Write_Diary.java
+++ app/src/main/java/com/example/withdiary/Write_Diary.java
@@ -22,11 +22,14 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.soundcloud.android.crop.Crop;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static android.content.RestrictionsManager.RESULT_ERROR;
 
 
 public class Write_Diary extends AppCompatActivity {
@@ -52,22 +55,19 @@ public class Write_Diary extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
                 public void onClick(View v) {
-                Intent intent = new Intent();
+                Crop.pickImage(Write_Diary.this);
+                /*Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 //startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
-                intent.putExtra("crop","true");
-                intent.putExtra("aspectX",0);
-                intent.putExtra("aspectY",200);
-                intent.putExtra("outputY",150);
-                intent.putExtra("outputX",150);
+
 
                 try{
                     intent.putExtra( "return-data",true );
                     startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
                 }catch(ActivityNotFoundException e) {
 
-                }
+                }*/
 
 
 
@@ -105,19 +105,38 @@ public class Write_Diary extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 0 && resultCode == RESULT_OK){
+        if(requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK){
             filePath = data.getData();
-            Log.d("MainActivity", "uri:" + String.valueOf(filePath));
+            Uri destinetion = Uri.fromFile( new File(getCacheDir(),"cropped"));
+            Crop.of( filePath,destinetion ).asSquare().start(this);
+            imageView.setImageURI( Crop.getOutput( data ) );
+            /*Log.d("MainActivity", "uri:" + String.valueOf(filePath));
             try{
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
                 imageView.setImageBitmap(bitmap);
             } catch (IOException e){
                 e.printStackTrace();
+            }*/
+        }
+            else if (requestCode==Crop.REQUEST_CROP)
+            {
+              handle_crop(resultCode,data);
             }
 
 
 
         }
+
+    private void handle_crop(int Code, Intent result) {
+        if(Code == RESULT_OK){
+            imageView.setImageURI( Crop.getOutput( result ) );
+
+
     }
+        else if (Code == RESULT_ERROR)
+        {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
+
+        }}
 
 }
